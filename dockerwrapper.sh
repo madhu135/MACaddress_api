a6ffe34ebeeb373791ef1a87175022148534308b
Madhu Komal <madhukomaln@hcl.com>
2019-06-16 03:04:21 +0000

    add all the project  files to the github

--- dockerwrapper.sh
+++ dockerwrapper.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-read -p "Please Enter Your APIKEY: "  APIKEY
-if [ -z "$APIKEY"  ]; then 
-     echo "Please enter the Apikey" 
+if [ "$1" = "-help" ]; then 
+     echo "$(<README.md)"
 else
-    for i in `cat macaddress.txt`;
-    do
+    read -p "Please Enter Your APIKEY: "  APIKEY
+    if [ -z "$APIKEY"  ]; then 
+     echo "Please enter the Apikeiy"
+    else 
+        for i in `cat macaddress.txt`;
+        do
          #Build the image from the Docker file and redirect the output to a file
            docker build -t test . >> /tmp/file.txt    
          #Run the container an pass the APIKEY and MACaddress as arugments through command line.
-          docker run test:latest  $APIKEY $i
-    done
+           docker run test:latest  $APIKEY $i
+           
+    
+        done
+     fi
 fi
                                
 
